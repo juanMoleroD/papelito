f9886740b85a8b86d5921fa003cff0a05d45e0b2
juanMoleroD <juanldimeco@gmail.com>
2023-11-16 17:25:33 +0000

    added spring project structure

---
+++ src/main/java/com/juanmolero/papelito/PapelitoApplication.java
@@ -0,0 +1,13 @@
+package com.juanmolero.papelito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapelitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PapelitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juanmolero/papelito/PapelitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanmolero.papelito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PapelitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
